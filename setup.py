6bd9098621f9bf3c5a77b6ee6f081d6ba3ffbec6
Ravi Kotecha <kotecha.ravi@gmail.com>
2013-02-07 15:33:23 +0000

    bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ except ImportError:
   from distutils.core import setup
 
 setup(name='pynliner',
-      version='0.4.0',
+      version='0.4.1',
       description='Python CSS-to-inline-styles conversion tool for HTML using BeautifulSoup and cssutils',
       author='Tanner Netterville',
       author_email='tannern@gmail.com',
